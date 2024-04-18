5b8c3618056ce2a7000de78fe07ab4da6a4ad1e8
Brandon Sneed <brandon@redf.net>
2024-04-18 09:55:44 -0700

    Amplitude sdk integration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,18 +19,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            name: "Segment",
-            url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.5.9"
-        )
+        .package(url: "https://github.com/segmentio/analytics-swift.git", from: "1.5.9"),
+        .package(url: "https://github.com/amplitude/Amplitude-Swift.git", from: "1.4.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentAmplitude",
-            dependencies: ["Segment"]),
+            dependencies: [
+                .product(name: "Segment", package: "analytics-swift"),
+                .product(name: "AmplitudeSwift", package: "Amplitude-Swift")
+            ]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
     ]
