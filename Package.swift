0f26b56941c5d86be56031ca7a79ac54746d723a
Didier Garcia <digarcia@twilio.com>
2026-07-29 08:30:26 -0400

    fix: bump tvOS minimum target to 13.0 to match analytics-swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SegmentAmplitude",
     platforms: [
         .iOS("13.0"),
-        .tvOS("11.0"),
+        .tvOS("13.0"),
         .watchOS("7.1"),
         .macOS(.v10_15)
     ],
